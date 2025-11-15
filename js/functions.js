809aa03b71d3e15eab63803d4c6959d57d8ee70c
Yash verma <yashverma9084+github@gmail.com>
2025-11-16 00:19:15 +0530

    commit

--- js/functions.js
+++ js/functions.js
@@ -1,40 +1,57 @@
-// function yash(){
-//     return 4+5;
+// // function yash(){
+// //     return 4+5;
+// // }
+// // const sum = yash();
+// // console.log(sum);
+
+
+
+// const age1 = (age)=>{
+//      if (age>=18) {
+//         return  "You are eligible for voting";
+//      }else{
+//          return "Not eligible for voting";
+//      }
 // }
-// const sum = yash();
-// console.log(sum);
+// console.log(age1(21));
 
-
-
-const age1 = (age)=>{
-     if (age>=18) {
-        return  "You are eligible for voting";
-     }else{
-         return "Not eligible for voting";
-     }
-}
-console.log(age1(21));
-
-function greet(name =" guest" ){
-    console.log("Hii "+name);
+// function greet(name =" guest" ){
+//     console.log("Hii "+name);
     
-}
-greet("yash");
-greet();
+// }
+// greet("yash");
+// greet();
 
-// Rest function
+// // Rest function
 
-function sum(num1,num2,...Rest) {
-    let total = num1+num2;
-    for (let index = 0; index < Rest.length; index++) {
-        total+=Rest[index];
+// function sum(num1,num2,...Rest) {
+//     let total = num1+num2;
+//     for (let index = 0; index < Rest.length; index++) {
+//         total+=Rest[index];
         
-    }
-    console.log(total);
+//     }
+//     console.log(total);
     
+// }
+
+// sum(2,3,4,5,6,7,7,8,9);
+// // console.log(sum(2,3,4,5,6,7,7,8,9));
+
+let arr = [-9, 7, 1, -2, 5, -1, -4, 2, -6, 3, 9, -7, 8, -8, 4];
+
+let j = 0; // points to position where next negative number should go
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i] < 0) {
+    // swap arr[i] with arr[j]
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+
+    j++;
+  }
 }
 
-sum(2,3,4,5,6,7,7,8,9);
-// console.log(sum(2,3,4,5,6,7,7,8,9));
+console.log(arr);
 
-
+console.log(arr);
